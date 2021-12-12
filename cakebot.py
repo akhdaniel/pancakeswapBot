6a4abdb3604df0caee267fe730c0679f5e13a6b0
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-12-13 06:09:14 +0700

    pancakeswap2_txn

--- cakebot.py
+++ cakebot.py
@@ -14,7 +14,8 @@ humanReadable = web3.fromWei(balance,'ether')
 print('balance: ',humanReadable)
  
 #Contract Address of Token we want to buy
-tokenToBuy = web3.toChecksumAddress(input("Enter TokenAddress: "))            
+# tokenToBuy = web3.toChecksumAddress(input("Enter TokenAddress: "))            
+tokenToBuy = web3.toChecksumAddress(config.TOKEN_TO_BUY)            
 #web3.toChecksumAddress("0x6615a63c260be84974166a5eddff223ce292cf3d")
 spend = web3.toChecksumAddress(config.BUSD)  #wbnb contract
  
@@ -28,7 +29,12 @@ pancakeswap2_txn = contract.functions.swapExactETHForTokens(
     [spend, tokenToBuy],
     sender_address,
     (int(time.time()) + 10000)
-    ).buildTransaction({
+    )
+
+totalgas=pancakeswap2_txn.estimateGas()
+print('total gas', totalgas)
+
+pancakeswap2_txn.buildTransaction({
     'from': sender_address,
     'value': web3.toWei(0.01,'ether'), #This is the Token(BUSD) amount you want to Swap from
     'gas': 250000,
