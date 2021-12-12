a681e1de94e8f2129ecb3e5807803b0bf7e22180
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-12-13 06:37:04 +0700

    approve

--- cakebot.py
+++ cakebot.py
@@ -3,6 +3,22 @@ from web3 import Web3
 import config
 import time
 
+
+
+def approve(amount):
+    busd_contract = web3.eth.contract(address=config.BUSD, abi=config.BUSD_ABI)
+    
+    #approve BUSD
+    txn = {
+        'chainId': config.eth_chain_id,
+        'nonce': nonce
+    }    
+    approve_txn = busd_contract.functions.approve(Web3.toChecksumAddress(config.sender_address),amount).buildTransaction(txn)
+    signed_txn = web3.eth.account.signTransaction(approve_txn, config.private)
+    tx_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    print('apprive txhash', tx_hash)
+
+
 bsc = "https://bsc-dataseed.binance.org/"
 web3 = Web3(Web3.HTTPProvider(bsc))
 
@@ -25,6 +41,10 @@ nonce = web3.eth.get_transaction_count(sender_address)
  
 start = time.time()
 
+amount=0.01
+approve(amount)
+
+#execute swap
 pancakeswap2_txn = contract.functions.swapExactETHForTokens(
     10000000000, # set to 0, or specify minimum amount of tokeny you want to receive - consider decimals!!!
     [spend, tokenToBuy],
@@ -32,7 +52,7 @@ pancakeswap2_txn = contract.functions.swapExactETHForTokens(
     (int(time.time()) + 10000)
 ).buildTransaction({
     'from': sender_address,
-    'value': web3.toWei(0.001,'ether'), #This is the Token(BUSD) amount you want to Swap from
+    'value': web3.toWei(amount,'ether'), #This is the Token(BUSD) amount you want to Swap from
     # 'gas': 250000,
     # 'gasPrice': web3.toWei('5','gwei'),
     'nonce': nonce
@@ -43,3 +63,4 @@ print('time duration: ', end-start)
 signed_txn = web3.eth.account.sign_transaction(pancakeswap2_txn, private_key=config.private)
 tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
 print('tx_hash',web3.toHex(tx_token))
+
