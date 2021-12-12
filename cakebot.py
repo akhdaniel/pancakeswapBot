fa0b7f4a70e4a0ee3eb80d3080263144944693be
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2021-12-13 06:16:37 +0700

    Update cakebot.py

--- cakebot.py
+++ cakebot.py
@@ -35,7 +35,7 @@ pancakeswap2_txn = contract.functions.swapExactETHForTokens(
     'value': web3.toWei(0.001,'ether'), #This is the Token(BUSD) amount you want to Swap from
     # 'gas': 250000,
     # 'gasPrice': web3.toWei('5','gwei'),
-    'nonce': nonce,
+    'nonce': nonce
 })
 
 end = time.time()
@@ -43,4 +43,3 @@ print('time duration: ', end-start)
 signed_txn = web3.eth.account.sign_transaction(pancakeswap2_txn, private_key=config.private)
 tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
 print('tx_hash',web3.toHex(tx_token))
-
